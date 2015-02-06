58f9d40b3922115f4d2d8049a2ce23e075fcca3e
Ryan S. Brown <sb@ryansb.com>
2015-02-05 20:02:12 -0500

    plot2 done

--- plot2.R
+++ plot2.R
@@ -1,6 +1,6 @@
 t <- read.table("chopped.txt", header=TRUE, sep=";")
-t$Date <- as.Date(strptime(t$Date, "%d/%M/%Y"))
 
-png("./plot1.png")
+png("./plot2.png")
 hist(t$Global_active_power, xlab="Global Active Power (kilowatts)", ylab="Frequency", col="red", main="Global Active Power")
+plot(as.POSIXct(paste(t$Date, t$Time), format="%d/%m/%Y %H:%M:%S"), t$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()
