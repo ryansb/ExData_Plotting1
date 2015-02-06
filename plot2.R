2d35127b77018d33032dd6b2daa007c6c8c9d0a9
Ryan S. Brown <sb@ryansb.com>
2015-02-05 20:52:23 -0500

    fix plot2.R

--- plot2.R
+++ plot2.R
@@ -1,6 +1,5 @@
 t <- read.table("chopped.txt", header=TRUE, sep=";")
 
 png("./plot2.png")
-hist(t$Global_active_power, xlab="Global Active Power (kilowatts)", ylab="Frequency", col="red", main="Global Active Power")
 plot(as.POSIXct(paste(t$Date, t$Time), format="%d/%m/%Y %H:%M:%S"), t$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()
